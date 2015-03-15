7607a421dcfbbea7cc300ddd080fd13b376704f9
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-15 13:29:39 +0000

    Add setup script for kittydar.

---
+++ workloads/setup/setup_kittydar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install nodejs npm libcairo2-dev libjpeg8-dev libpango1.0-dev libgif-dev
+
+cd workloads/kittydar/
+npm install nomnom canvas colors charm hog-descriptor svm brain
