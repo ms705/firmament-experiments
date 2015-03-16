82f8ed6e30b3c25e6e1b9ff3e44625332d9a693e
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-16 16:17:58 +0000

    Fix typo... whoops.

--- scripts/setup_10g.sh
+++ scripts/setup_10g.sh
@@ -4,7 +4,7 @@
 IP=$(ifconfig em1 | grep "inet addr" | cut -d'.' -f4 | cut -d' ' -f1)
 
 # set up the 10G interface
-sudo ifconfig up p1p1
+sudo ifconfig p1p1 up
 sudo ifconfig p1p1 10.0.0.${IP}/24
 
 if [[ $(ifconfig p1p1 | grep UP) == "" ]]; then 
