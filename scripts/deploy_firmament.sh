d03d3a42f776bd85653a9ad5611e7c19a917b42e
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-16 16:15:24 +0000

    Install Linux perf tool packages when deploying.

--- scripts/deploy_firmament.sh
+++ scripts/deploy_firmament.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# required for perf support
+sudo apt-get -y install linux-tools-common
+sudo apt-get -y install linux-tools-`uname -r`
+
+# actual Firmament
 git clone https://github.com/ms705/firmament.git firmament
 cd firmament
 export NONINTERACTIVE=1
