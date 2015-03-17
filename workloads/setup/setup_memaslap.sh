e3508d3c8365da3347f1a549c7af61f6daaa9ef3
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-17 02:06:25 +0000

    Fix errors in memaslap setup script.

--- workloads/setup/setup_memaslap.sh
+++ workloads/setup/setup_memaslap.sh
@@ -2,6 +2,7 @@
 
 LIBMEMCACHED_VER=1.0.18
 
+mkdir -p workloads/memaslap
 cd workloads/memaslap
 wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-${LIBMEMCACHED_VER}.tar.gz
 tar -xvzf libmemcached-${LIBMEMCACHED_VER}.tar.gz
@@ -11,4 +12,8 @@ cd libmemcached-${LIBMEMCACHED_VER}
 LIBS=-lpthread ./configure --enable-memaslap
 make -j12
 
-echo [[ -f clients/memaslap ]]
+if [ -f clients/memaslap ]; then
+  exit 0
+else
+  exit 1
+fi
