a9767c0bbd3202435af46f039725d9caa943fa90
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-17 02:07:14 +0000

    Install Kazoo (required for napper).

--- scripts/deploy_firmament.sh
+++ scripts/deploy_firmament.sh
@@ -4,6 +4,9 @@
 sudo apt-get -y install linux-tools-common
 sudo apt-get -y install linux-tools-`uname -r`
 
+# required for napper
+sudo apt-get -y install python-kazoo
+
 # actual Firmament
 git clone https://github.com/ms705/firmament.git firmament
 cd firmament
