c7153009bb725f430d1e515709646d8eee21a071
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-18 22:57:31 +0000

    Stop services after package installation.

--- workloads/setup/setup_memcached.sh
+++ workloads/setup/setup_memcached.sh
@@ -4,3 +4,4 @@ sudo apt-get -y update
 sudo apt-get -y install memcached rcconf
 
 sudo rcconf --off memcached
+sudo service memcached stop
--- workloads/setup/setup_nginx.sh
+++ workloads/setup/setup_nginx.sh
@@ -4,3 +4,4 @@ sudo apt-get -y update
 sudo apt-get -y install nginx rcconf
 
 sudo rcconf --off nginx
+sudo service nginx stop
