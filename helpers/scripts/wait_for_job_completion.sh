c44ad75cb417d02d96398021d194b285f57e6c87
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-20 11:02:06 +0000

    Add script to poll for job completion.

---
+++ helpers/scripts/wait_for_job_completion.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [[ $# -lt 2 ]]; then
+  echo "usage: wait_for_job_completion.sh <job ID> <coordinator URL>"
+  exit 1
+fi
+
+JOB_ID=$1
+COORD_URL=$2
+
+STATUS=$(curl -s -L "${COORD_URL}/job/completion/?id=${JOB_ID}&json=1")
+
+while [[ ${STATUS} != "COMPLETED" ]]; do
+  echo "Still running..."
+  sleep 10
+  STATUS=$(curl -s -L "${COORD_URL}/job/completion/?id=${JOB_ID}&json=1")
+done
+
+echo "Done at $(date)!"
