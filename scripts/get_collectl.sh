3e74e46d6da6c31dbba67bc5e48b26420795fc60
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-03-23 18:59:58 +0000

    Fix check for the correct number of arguments.

--- scripts/get_collectl.sh
+++ scripts/get_collectl.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [[ $# -lt 4 ]]; then
-  echo "usage: get_collect.sh <date> <from> <to> <outdir>"
+if [[ $# -lt 3 ]]; then
+  echo "usage: get_collect.sh <date> <from> <to>"
   exit 1
 fi
 
 DATE=$1
 FROM=$2
 TO=$3
-OUTDIR=$4
 FILENAME="/var/log/collectl/$(hostname)-${DATE}-000000.raw.gz"
 
 rm /tmp/collectl-${FROM}-${TO}-$(hostname)-20150323.tab
