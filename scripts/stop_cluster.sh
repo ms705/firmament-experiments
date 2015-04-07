5a415fe9cc0b06766ba69ec7e62a56c3837284e9
Malte Schwarzkopf <ms705-git@srcf.ucam.org>
2015-04-07 13:26:55 +0100

    More advanced cluster start/stop scripts.

--- scripts/stop_cluster.sh
+++ scripts/stop_cluster.sh
@@ -5,5 +5,9 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
+echo "Killing coordinators..."
 parallel-ssh -t 0 -h $1 "killall coordinator"
 #parallel-ssh -t 0 -h $1 "curl http://localhost:8080/shutdown"
+
+echo "Cleaning up zombies..."
+parallel-ssh -t 0 -h $1 "fuser 8080/tcp | cut -d':' -f1 | xargs kill"
