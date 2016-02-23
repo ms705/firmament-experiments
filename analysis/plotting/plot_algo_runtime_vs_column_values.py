784bb2310fb463eb7a7d702749d2200fdea6aa62
Ionel Gog <ionel.gog@cl.cam.ac.uk>
2016-02-23 11:44:33 +0000

    Added script to plot algorithm runtime vs scheduler_events table column.

--- analysis/plotting/plot_algo_runtime_vs_column_values.py
+++ analysis/plotting/plot_algo_runtime_vs_column_values.py
@@ -19,26 +19,21 @@ gflags.DEFINE_string('trace_labels', '',
                      ', separated list of labels to use for trace files.')
 gflags.DEFINE_bool('ignore_first_run', True,
                    'True if the first run should be ignored')
+gflags.DEFINE_integer('column_index', 0, 'The column index in the '
+                      'scheduler_events table to plot runtime against')
+gflags.DEFINE_string('label', '', 'The label of the column')
 
-def get_algorithm_runtime_and_num_changes(trace_path):
-    # 7 nodes added
-    # 8 nodes removed
-    # 9 arcs added
-    # 10 arcs changed
-    # 11 arcs removed
+
+def get_algorithm_runtime_and_column(trace_path, column_index):
     csv_file = open(trace_path + "/scheduler_events/scheduler_events.csv")
     csv_reader = csv.reader(csv_file)
     runtimes = []
     num_graph_changes = []
     for row in csv_reader:
         runtimes.append(long(row[2]))
-        num_graph_changes.append(long(row[7]) + long(row[8]) + long(row[9]) +
-                                 long(row[10]) + long(row[11]))
+        num_graph_changes.append(long(row[column_index]))
     csv_file.close()
-    if FLAGS.ignore_first_run is True:
-        return (runtimes[1:], num_graph_changes[1:])
-    else:
-        return (runtimes, num_graph_changes)
+    return (runtimes[1:], num_graph_changes[1:])
 
 
 def plot_scatter(plot_file_name, x_vals, y_vals, x_label, y_label):
@@ -69,14 +64,15 @@ def main(argv):
     labels = FLAGS.trace_labels.split(',')
 
     if len(trace_paths) != 1:
-        print 'Error: cannot plot algorithm vs num_changes for > 1 trace'
+        print 'Error: cannot plot for more than a trace'
         exit(1)
 
-    (runtimes, num_graph_changes) = get_algorithm_runtime_and_num_changes(trace_paths[0])
+    (runtimes, col_vals) = get_algorithm_runtime_and_column(trace_paths[0],
+                                                            FLAGS.column_index)
     print "Number scheduler runs: %d" % (len(runtimes))
-    plot_scatter('algorithm_runtime_vs_num_changes',
-                 [x / 1000 for x in runtimes], num_graph_changes,
-                 'Runtime [ms]', 'Number graph changes')
+    plot_scatter('algorithm_runime_vs_column',
+                 [x / 1000 for x in runtimes], col_vals,
+                 'Runtime [ms]', FLAGS.label)
 
 
 if __name__ == '__main__':
