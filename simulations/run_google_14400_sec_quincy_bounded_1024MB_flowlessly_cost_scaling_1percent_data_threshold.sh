183504066a59fec75f50f0ab10679fa3cebd3b88
Ionel Gog <gogionel@gmail.com>
2016-03-30 15:29:50 -0400

    Added run scripts for 4h Quincy simulations.

---
+++ simulations/run_google_14400_sec_quincy_bounded_1024MB_flowlessly_cost_scaling_1percent_data_threshold.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd firmament
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_14400_sec_quincy_flowlessly_cost_scaling_1percent_data_threshold.cfg
---
+++ simulations/run_google_14400_sec_quincy_bounded_1024MB_flowlessly_incremental_cost_scaling_then_relax.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd firmament
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_14400_sec_quincy_flowlessly_incremental_cost_scaling_then_relax.cfg
