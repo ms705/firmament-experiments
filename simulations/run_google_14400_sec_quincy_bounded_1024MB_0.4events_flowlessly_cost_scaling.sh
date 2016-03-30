07b4feb87fe27b1f0c9264851aae03c54375840b
Ionel Gog <gogionel@gmail.com>
2016-03-30 16:54:25 -0400

    Added config and run scripts for Quincy on fraction of Google trace.

---
+++ simulations/run_google_14400_sec_quincy_bounded_1024MB_0.4events_flowlessly_cost_scaling.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd firmament
+mkdir /mnt/data/google_14400_sec_quincy_bounded_1024MB_0.4events_flowlessly_cost_scaling/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_14400_sec_quincy_bounded_1024MB_0.4events_flowlessly_cost_scaling.cfg
