5cf13bdbe2a23849480c80743aa4c9f6ee3dca5e
Ionel Gog <gogionel@gmail.com>
2016-04-04 19:47:14 -0400

    Added more scripts to generate plots.

--- analysis/plotting/generate_plots.sh
+++ analysis/plotting/generate_plots.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
-ssh ganymede.cl.cam.ac.uk './mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scheduling_runtimes.sh'
+# Scheduler & Algorithm runtime CDFs for various solvers
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scheduling_runtimes.sh'
 
+# Quincy scalability
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scalability_cluster.sh'
+
+# Quincy percentage of evicted tasks
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_percentage_evicted_tasks.sh'
+
+# DFS plots
+ssh ganymede.cl.cam.ac.uk 'cd /mnt/data/icg27/firmament-experiments/analysis/plotting/ ; python plot_dfs_machine_data_cdf.py --trace_path=/mnt/data/icg27/firmament_simulations/old_quincy_pinned/google_14400_sec_quincy_bounded_1024MB_flowlessly_cost_scaling/'
+
+ssh ganymede.cl.cam.ac.uk 'cd /mnt/data/icg27/firmament-experiments/analysis/plotting/ ; python plot_dfs_task_input_data_cdf.py --trace_path=/mnt/data/icg27/firmament_simulations/old_quincy_pinned/google_14400_sec_quincy_bounded_1024MB_flowlessly_cost_scaling/'
+
+ssh ganymede.cl.cam.ac.uk 'cd /mnt/data/icg27/firmament-experiments/analysis/plotting/ ; python plot_input_size_vs_num_arc_preferences.py --trace_path=/mnt/data/icg27/firmament_simulations/old_quincy_pinned/google_14400_sec_quincy_bounded_1024MB_flowlessly_cost_scaling/'
+
+# Copy the pdfs
 scp ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament-experiments/analysis/plotting/*.pdf ./
