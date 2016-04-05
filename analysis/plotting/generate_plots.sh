a21d34a4ae22d91e0d4f7340ab60195200292348
Ionel Gog <gogionel@gmail.com>
2016-04-05 14:36:34 -0400

    Added scripts to generate preemption plots.

--- analysis/plotting/generate_plots.sh
+++ analysis/plotting/generate_plots.sh
@@ -2,12 +2,15 @@
 
 # Scheduler & Algorithm runtime CDFs for various solvers
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scheduling_runtimes.sh 14400'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_scheduling_runtimes.sh 14400'
 
 # Quincy scalability
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scalability_cluster.sh 14400'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_scalability_cluster.sh 14400'
 
 # Quincy percentage of evicted tasks
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_percentage_evicted_tasks.sh 14400'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_percentage_evicted_tasks.sh 14400'
 
 # DFS plots
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_dfs_plots.sh 14400'
@@ -17,12 +20,15 @@ ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotti
 
 # Scheduler backlog timeline
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scheduling_backlog_timeline.sh 14400'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_scheduling_backlog_timeline.sh 14400'
 
 # Num changes vs runtime
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_algorithm_runtime_vs_num_changes.sh 14400'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_algorithm_runtime_vs_num_changes.sh 14400'
 
 # Scheduling delay
 ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_pinned_scheduling_delay.sh 14400 1'
+ssh ganymede.cl.cam.ac.uk '/mnt/data/icg27/firmament-experiments/analysis/plotting/generate_quincy_preemption_scheduling_delay.sh 14400 1'
 
 # Percentage data locality
 # TODO(ionel): Add!
