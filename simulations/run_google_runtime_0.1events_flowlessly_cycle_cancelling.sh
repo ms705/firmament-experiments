732ee3a0c1f2158a40375bd3a3a4c8cf377923b5
Ionel Gog <gogionel@gmail.com>
2016-04-12 13:18:37 -0400

    Added configs and scripts for algorithm runtime experiments.

---
+++ simulations/run_google_runtime_0.1events_flowlessly_cycle_cancelling.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_runtime_0.1events_flowlessly_cycle_cancelling/
+mkdir /mnt/data/google_runtime_0.1events_flowlessly_cycle_cancelling/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_runtime_0.1events_flowlessly_cycle_cancelling.cfg
+scp -r /mnt/data/google_runtime_0.1events_flowlessly_cycle_cancelling/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
