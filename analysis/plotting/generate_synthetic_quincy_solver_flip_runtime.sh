1d3795f188bc15909ebd7f9cb3388a6c9b746f88
Ionel Gog <gogionel@gmail.com>
2016-04-15 18:32:59 +0100

    Added script to generate graphs for synthetic solver flip.

---
+++ analysis/plotting/generate_synthetic_quincy_solver_flip_runtime.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd /mnt/data/icg27/firmament-experiments/analysis/plotting/
+python plot_solver_flip_runtime_cdf.py --trace_paths=/mnt/data/icg27/firmament_simulations/quincy/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine,/mnt/data/icg27/firmament_simulations/quincy/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip --trace_labels="solver flip,solver flip + price refine" --paper_mode --log_scale=True --algorithm=relax
+
+mv scheduling_runtimes_cdf-90th.pdf synthetic_quincy_relax_solver_flip_scheduling_runtimes_cdf-90th.pdf
+mv scheduling_runtimes_cdf-99th.pdf synthetic_quincy_relax_solver_flip_scheduling_runtimes_cdf-99th.pdf
+mv scheduling_runtimes_cdf.pdf synthetic_quincy_relax_solver_flip_scheduling_runtimes_cdf.pdf
+mv algorithm_runtimes_cdf-90th.pdf synthetic_quincy_relax_solver_flip_runtimes_cdf-90th.pdf
+mv algorithm_runtimes_cdf-99th.pdf synthetic_quincy_relax_solver_flip_runtimes_cdf-99th.pdf
+mv algorithm_runtimes_cdf.pdf synthetic_quincy_relax_solver_flip_runtimes_cdf.pdf
+
+
+python plot_solver_flip_runtime_cdf.py --trace_paths=/mnt/data/icg27/firmament_simulations/quincy/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine,/mnt/data/icg27/firmament_simulations/quincy/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip --trace_labels="solver flip,solver flip + price refine" --paper_mode --log_scale=False --algorithm=cost_scaling
+
+mv scheduling_runtimes_cdf-90th.pdf synthetic_quincy_cost_scaling_solver_flip_scheduling_runtimes_cdf-90th.pdf
+mv scheduling_runtimes_cdf-99th.pdf synthetic_quincy_cost_scaling_solver_flip_scheduling_runtimes_cdf-99th.pdf
+mv scheduling_runtimes_cdf.pdf synthetic_quincy_cost_scaling_solver_flip_scheduling_runtimes_cdf.pdf
+mv algorithm_runtimes_cdf-90th.pdf synthetic_quincy_cost_scaling_solver_flip_runtimes_cdf-90th.pdf
+mv algorithm_runtimes_cdf-99th.pdf synthetic_quincy_cost_scaling_solver_flip_runtimes_cdf-99th.pdf
+mv algorithm_runtimes_cdf.pdf synthetic_quincy_cost_scaling_solver_flip_runtimes_cdf.pdf
