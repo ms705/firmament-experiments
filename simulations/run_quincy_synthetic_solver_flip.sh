9477023ada7cd8bb28678872d5b1a280f32b7344
Ionel Gog <gogionel@gmail.com>
2016-04-15 13:03:30 -0400

    Fixed config file.

--- simulations/run_quincy_synthetic_solver_flip.sh
+++ simulations/run_quincy_synthetic_solver_flip.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ssh srguser@caelum-304.cl.cam.ac.uk 'screen -d -m /home/srguser/firmament-experiments/simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip.sh'
-ssh srguser@caelum-305.cl.cam.ac.uk 'screen -d -m /home/srguser/firmament-experiments/simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine.sh'
+ssh srguser@caelum-409.cl.cam.ac.uk 'screen -d -m /home/srguser/firmament-experiments/simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip.sh'
+ssh srguser@caelum-410.cl.cam.ac.uk 'screen -d -m /home/srguser/firmament-experiments/simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine.sh'
