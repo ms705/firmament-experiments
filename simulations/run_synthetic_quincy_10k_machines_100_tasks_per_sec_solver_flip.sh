8189ac91a31c63b70abb2fe2f98ecce07ef6dd5c
Ionel Gog <gogionel@gmail.com>
2016-04-15 13:10:48 -0400

    Fixed scripts.

--- simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip.sh
+++ simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd firmament
+cd ~/firmament/build/third_party/flowlessly/src/flowlessly/
+git checkout master
+# Solver flip with price refine.
+git checkout d7fc6ef7ef055d8f084a54e5f619303cfad979d0
+cmake .
+make clean; make -j3
+cd ~/firmament
 rm -r /mnt/data/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip/
 mkdir /mnt/data/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip/
 ./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip.cfg
--- simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine.sh
+++ simulations/run_synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd firmament
+cd ~/firmament/build/third_party/flowlessly/src/flowlessly/
+git checkout master
+# Solver flip without price refine.
+git checkout c364d168adcafb8927961cc7cc791527fc6440dd
+cmake .
+make clean; make -j3
+cd ~/firmament
 rm -r /mnt/data/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine/
 mkdir /mnt/data/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine/
 ./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/synthetic_quincy_10k_machines_100_tasks_per_sec_solver_flip_without_refine.cfg
