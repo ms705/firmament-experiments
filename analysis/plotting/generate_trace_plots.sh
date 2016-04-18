ea5c5b0f3d9eff1155002cadffa61d33a82f9fb6
Ionel Gog <gogionel@gmail.com>
2016-04-18 22:27:28 +0100

    Updated generate trace plots command.

--- analysis/plotting/generate_trace_plots.sh
+++ analysis/plotting/generate_trace_plots.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-python plot_scheduling_events_per_time_interval_cdf.py --trace_path=/data/flowlessly/google_trace/  --num_files_to_process=500 --time_intervals='500000,750000,1000000,2000000,4000000,6000000,8000000,10000000,20000000' --time_labels='500 ms,750 ms,1000 ms,2000 ms,4000 ms,6000 ms,8000 ms,10000 ms,20000 ms'
+python plot_scheduling_events_per_time_interval_cdf.py --trace_path=/data/flowlessly/google_trace/  --num_files_to_process=500 --time_intervals='500000,1000000,2000000,5000000,10000000,20000000' --time_labels='500 ms,1000 ms,2000 ms,5000 ms,10000 ms,20000 ms' --paper_mode
 
-python plot_machine_removals_per_time_interval.py  --trace_path=/data/flowlessly/google_trace/ --time_interval=100000000
+python plot_machine_removals_per_time_interval.py  --trace_path=/data/flowlessly/google_trace/ --time_interval=100000000 --paper_mode
