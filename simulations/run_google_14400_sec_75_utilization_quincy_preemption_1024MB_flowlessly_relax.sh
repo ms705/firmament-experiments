2b7eef694c5b6db775de30a165b4db0ee218c889
Ionel Gog <gogionel@gmail.com>
2016-04-26 10:34:15 -0400

    Added cluster utilization scripts.

---
+++ simulations/run_google_14400_sec_75_utilization_quincy_preemption_1024MB_flowlessly_relax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_14400_sec_75_utilization_quincy_preemption_bounded_1024MB_flowlessly_relax/
+mkdir /mnt/data/google_14400_sec_75_utilization_quincy_preemption_bounded_1024MB_flowlessly_relax/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_14400_sec_75_utilization_quincy_preemption_bounded_1024MB_flowlessly_relax.cfg
+scp -r /mnt/data/google_14400_sec_75_utilization_quincy_preemption_bounded_1024MB_flowlessly_relax/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
