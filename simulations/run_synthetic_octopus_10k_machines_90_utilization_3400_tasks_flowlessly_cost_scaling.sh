8e740409b75d1b7bda3da75d834fa01100e7c5f0
Ionel Gog <gogionel@gmail.com>
2016-04-27 15:07:20 -0400

    More synthetic runs.

---
+++ simulations/run_synthetic_octopus_10k_machines_90_utilization_3400_tasks_flowlessly_cost_scaling.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/synthetic_octopus_10k_machines_90_utilization_3400_tasks_flowlessly_cost_scaling/
+mkdir /mnt/data/synthetic_octopus_10k_machines_90_utilization_3400_tasks_flowlessly_cost_scaling/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/synthetic_octopus_10k_machines_90_utilization_3400_tasks_flowlessly_cost_scaling.cfg
+scp -r /mnt/data/synthetic_octopus_10k_machines_90_utilization_3400_tasks_flowlessly_cost_scaling/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
---
+++ simulations/run_synthetic_octopus_10k_machines_90_utilization_3800_tasks_flowlessly_relax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/synthetic_octopus_10k_machines_90_utilization_3800_tasks_flowlessly_relax/
+mkdir /mnt/data/synthetic_octopus_10k_machines_90_utilization_3800_tasks_flowlessly_relax/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/synthetic_octopus_10k_machines_90_utilization_3800_tasks_flowlessly_relax.cfg
+scp -r /mnt/data/synthetic_octopus_10k_machines_90_utilization_3800_tasks_flowlessly_relax/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
