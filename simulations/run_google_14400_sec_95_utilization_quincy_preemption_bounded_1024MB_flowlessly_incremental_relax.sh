b3bdeb59bf46c82a2e162870287a52c2e133269b
Ionel Gog <gogionel@gmail.com>
2016-04-28 18:39:31 -0400

    Added more experiments.

---
+++ simulations/run_google_14400_sec_95_utilization_quincy_preemption_bounded_1024MB_flowlessly_incremental_relax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~/firmament
+rm -r /mnt/data/google_14400_sec_95_utilization_quincy_preemption_bounded_1024MB_flowlessly_incremental_relax/
+mkdir /mnt/data/google_14400_sec_95_utilization_quincy_preemption_bounded_1024MB_flowlessly_incremental_relax/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_14400_sec_95_utilization_quincy_preemption_bounded_1024MB_flowlessly_incremental_relax.cfg
+scp -r /mnt/data/google_14400_sec_95_utilization_quincy_preemption_bounded_1024MB_flowlessly_incremental_relax/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
