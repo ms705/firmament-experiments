71d436ad134bc962f9e9eed2c44155909c87f638
Ionel Gog <gogionel@gmail.com>
2016-04-29 10:19:06 -0400

    More configs.

---
+++ simulations/run_google_synthetic_quincy_13pus_5000_tasks_flowlessly_relax.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_synthetic_quincy_13pus_5000_tasks_flowlessly_relax/
+mkdir /mnt/data/google_synthetic_quincy_13pus_5000_tasks_flowlessly_relax/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_synthetic_quincy_13pus_5000_tasks_flowlessly_relax.cfg
+scp -r /mnt/data/google_synthetic_quincy_13pus_5000_tasks_flowlessly_relax/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
