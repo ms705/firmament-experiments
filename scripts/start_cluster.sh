c63e5368e6bb85b1727064909465c29eba2dfdbc
Ionel Gog <gogionel@gmail.com>
2016-05-06 16:32:33 -0400

    Updated start_cluster script.

--- scripts/start_cluster.sh
+++ scripts/start_cluster.sh
@@ -18,8 +18,8 @@ if [[ $# -eq 4 ]]; then
 fi
 
 # start local coordinator
-build/engine/coordinator --listen_uri=tcp:$2:9999 --task_lib_dir=${ROOT}/firmament/build/engine/ --scheduler=${SCHEDULER} ${COST_MODEL} --log_dir=${LOG_DIR} --task_log_dir=${ROOT}/firmament-logs/ --task_perf_dir=${ROOT}/firmament-perf --flow_scheduling_solver=cs2 --debug_flow_graph --task_data_dir=/mnt/scratch/ --pin_tasks_to_cores=true --perf_monitoring=false --cs2_binary=ext/cs2-git/cs2.exe &
+build/src/coordinator --listen_uri=tcp:$2:9999 --task_lib_dir=${ROOT}/firmament/build/src/ --scheduler=${SCHEDULER} ${COST_MODEL} --log_dir=${LOG_DIR} --task_log_dir=${ROOT}/firmament-logs/ --task_perf_dir=${ROOT}/firmament-perf --flow_scheduling_solver=cs2 --debug_flow_graph --task_data_dir=/mnt/scratch/ --pin_tasks_to_cores=true --preemption=false --perf_monitoring=false &
 sleep 1
 
 # start other coordinators and link them into local one
-parallel-ssh -t 0 -h $1 "cd firmament; build/engine/coordinator --parent_uri=tcp:$2:9999 --task_lib_dir=${ROOT}/firmament/build/engine/ --scheduler=simple --log_dir=${LOG_DIR} --task_log_dir=${ROOT}/firmament-logs/ --task_perf_dir=${ROOT}/firmament-perf --task_data_dir=/mnt/scratch/ --pin_tasks_to_cores=true --perf_monitoring=false"
+parallel-ssh -t 0 -h $1 "cd firmament; build/src/coordinator --parent_uri=tcp:$2:9999 --task_lib_dir=${ROOT}/firmament/build/src/ --scheduler=${SCHEDULER} ${COST_MODEL} --flow_scheduling_solver=cs2 --log_dir=${LOG_DIR} --task_log_dir=${ROOT}/firmament-logs/ --task_perf_dir=${ROOT}/firmament-perf --task_data_dir=/mnt/scratch/ --pin_tasks_to_cores=true --preemption=false --perf_monitoring=false"
