9169d96dc770b1655b81c2bbb2984d5feab987f3
Ionel Gog <gogionel@gmail.com>
2016-05-20 21:34:05 +0100

    Updated plotting scripts & added hdfs_get baseline script.

---
+++ analysis/plotting/generate_colocation_plot_slides.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python plot_cluster_mix.py /mnt/data/icg27/firmament-experiments/results/synthetic/simple/caelum_3600s_0/ simple /mnt/data/icg27/firmament-experiments/results/synthetic/mesos/caelum_3600s_2/ mesos /mnt/data/icg27/firmament-experiments/results/synthetic/coco/caelum_3600s_6/ coco colocation_box_and_whiskers.pdf
---
+++ workloads/synthetic/hdfs/run_all_hdfs_gets.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+/usr/bin/time -a --output=hdfs_baseline_time -f "%E" ./hdfs_get caelum10g-301.cl.cam.ac.uk 8020 /input/test_data/task_runtime_events.csv
+
+for i in `seq 0 7` ; do /usr/bin/time -a --output=hdfs_baseline_time -f "%E" ./hdfs_get caelum10g-301.cl.cam.ac.uk 8020 /input/sssp_tw_edges_splits8/sssp_tw_edges${i}.in ; done
+
+for i in `seq 0 15` ; do /usr/bin/time -a --output=hdfs_baseline_time -f "%E" ./hdfs_get caelum10g-301.cl.cam.ac.uk 8020 /input/pagerank_uk-2007-05_edges_splits16/pagerank_uk-2007-05_edges${i}.in ; done
+
+for i in `seq 0 13` ; do /usr/bin/time -a --output=hdfs_baseline_time -f "%E" ./hdfs_get caelum10g-301.cl.cam.ac.uk 8020 /input/lineitem_splits14/lineitem${i}.in ; done
+
+/usr/bin/time -a --output=hdfs_baseline_time -f "%E" ./hdfs_get caelum10g-301.cl.cam.ac.uk 8020 /input/test_data/task_runtime_events.csv
