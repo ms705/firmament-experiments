b0253fb4ed869f28a12f3da03cc53020ffbf0c86
Ionel Gog <gogionel@gmail.com>
2016-05-28 16:20:18 -0400

    Added run scripts for cluster util incremental cost scaling.

---
+++ simulations/run_google_synthetic_quincy_13pus_15000_tasks_flowlessly_incremental_cost_scaling.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_synthetic_quincy_13pus_15000_tasks_flowlessly_incremental_cost_scaling/
+mkdir /mnt/data/google_synthetic_quincy_13pus_15000_tasks_flowlessly_incremental_cost_scaling/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_synthetic_quincy_13pus_15000_tasks_flowlessly_incremental_cost_scaling.cfg
+scp -r /mnt/data/google_synthetic_quincy_13pus_15000_tasks_flowlessly_incremental_cost_scaling/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
