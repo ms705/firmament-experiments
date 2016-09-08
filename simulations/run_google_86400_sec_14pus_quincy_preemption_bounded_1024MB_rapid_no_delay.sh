f45bce3d123b6b9e4199db2fa21a906087da06bd
Ionel Gog <ionel.gog@cl.cam.ac.uk>
2016-09-08 15:32:27 +0100

    Added speed up script files with 14pus.

---
+++ simulations/run_google_86400_sec_14pus_quincy_preemption_bounded_1024MB_rapid_no_delay.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_86400_sec_14pus_quincy_preemption_bounded_1024MB_rapid_no_delay/
+mkdir /mnt/data/google_86400_sec_14pus_quincy_preemption_bounded_1024MB_rapid_no_delay/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_86400_sec_14pus_quincy_preemption_bounded_1024MB_rapid_no_delay.cfg
+scp -r /mnt/data/google_86400_sec_14pus_quincy_preemption_bounded_1024MB_rapid_no_delay/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
