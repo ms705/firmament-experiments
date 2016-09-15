0c2dd497dfa2f98b50a72a8f3db1105f6c738867
Ionel Gog <gogionel@gmail.com>
2016-09-15 09:16:58 +0100

    Added more speedup configs.

---
+++ simulations/run_google_200x_speedup_16pus_86400_sec_quincy_preemption_bounded_1024MB_flowlessly_cost_scaling.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd firmament
+rm -r /mnt/data/google_200x_speedup_16pus_86400_sec_quincy_preemption_bounded_1024MB_flowlessly_cost_scaling/
+mkdir /mnt/data/google_200x_speedup_16pus_86400_sec_quincy_preemption_bounded_1024MB_flowlessly_cost_scaling/
+./build/src/simulator --flagfile=/home/srguser/firmament-experiments/configs/google_200x_speedup_16pus_86400_sec_quincy_preemption_bounded_1024MB_flowlessly_cost_scaling.cfg
+scp -r /mnt/data/google_200x_speedup_16pus_86400_sec_quincy_preemption_bounded_1024MB_flowlessly_cost_scaling/ icg27@ganymede.cl.cam.ac.uk:/mnt/data/icg27/firmament_simulations/
